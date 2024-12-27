60e0f1268c76480ba2f0b6d32d69512011ae11ae
baobaybong <baorng@gmail.com>
2024-12-27 17:41:56 +0100

    Add yazi theme, cp fish abbreviations and nvim init

---
+++ home/private_dot_config/nvim/init.lua
@@ -0,0 +1,12 @@
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+end
+
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
