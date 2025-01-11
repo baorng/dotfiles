d135f0b7472deb801c8407be949dd18087b3bc5c
baobaybong <baorng@gmail.com>
2025-01-11 10:43:29 +0100

    Use win g++ instead for faster compile time in CP

--- home/private_dot_config/fish/conf.d/abbr.fish
+++ home/private_dot_config/fish/conf.d/abbr.fish
@@ -1,4 +1,7 @@
 abbr -a cat 'bat'
 abbr -a gs 'git status'
-abbr -a c 'g++ -std=c++20 -O2 -Wall -Wextra -Wshadow -DLOCAL -o a' 
-abbr -a a './a'
+
+# Competitive programming abbreviations
+# Using .exe since my g++ on Windows somehow compiles 2x faster
+abbr -a c 'g++.exe -std=c++20 -O2 -Wall -Wextra -Wshadow -DLOCAL -o a' 
+abbr -a a './a.exe'
