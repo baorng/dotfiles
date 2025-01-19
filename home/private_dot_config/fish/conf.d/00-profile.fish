0a4cc80b31b04a6d5835b88d0b827707399310ee
baobaybong <baorng@gmail.com>
2025-01-19 05:25:08 +0100

    Fix wl-clip missing runtime on WSL, workaround for helix not resetting cursor shape

--- home/private_dot_config/fish/conf.d/00-profile.fish
+++ home/private_dot_config/fish/conf.d/00-profile.fish
@@ -2,6 +2,10 @@
 set -qg XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
 set -qg XDG_DATA_HOME; or set -gx XDG_DATA_HOME $HOME/.local/share
 set -qg XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
+# Fix for wsl wl-clip missing runtime
+if test -d /mnt/wslg/runtime-dir
+    set -gx XDG_RUNTIME_DIR /mnt/wslg/runtime-dir
+end
 
 # Favor globals for most things
 # set -gx EDITOR nvim
@@ -13,8 +17,15 @@ set -gx VISUAL hx
 # Paths
 fish_add_path -g ~/.local/bin
 
-# Temporary fix for code in fish-shell/fish-beta-4 WSL
+# FIXME: Temporary fix for code in fish-shell/fish-beta-4 WSL
 set code_path (which code)
 if test -x $code_path
     fish_add_path -g (dirname "$code_path")
+end
+
+# FIXME: remove once https://github.com/helix-editor/helix/issues/10089 is fixed
+# resets terminal cursor shape on exit
+function hx
+    command hx $argv
+    printf '\033[0 q'
 end
--- home/private_dot_config/fish/functions/y.fish
+++ home/private_dot_config/fish/functions/y.fish
@@ -1,9 +1,12 @@
 # yazi shell wrapper to cd
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+
+    # In case some program forgets to reset cursor shape
+    printf '\033[0 q'
 end
