8a5d7c109026b569912b46ad4f75ee329c657e30
baorng <baorng@gmail.com>
2025-12-24 21:17:50 +0700

    Add git abbrs back

--- home/private_dot_config/fish/conf.d/02-abbr.fish
+++ home/private_dot_config/fish/conf.d/02-abbr.fish
@@ -13,13 +13,33 @@ end
 #     abbr -a vim nvim
 # end
 
-# abbr -a gs 'git status'
-# abbr -a gd 'git diff'
-# abbr -a ga 'git add'
-# abbr -a gc 'git commit'
-# abbr -a gb 'git branch'
-# abbr -a gco 'git checkout'
-# abbr -a gr 'git rev-parse --show-toplevel'
+# Git abbreviations
+# Core
+abbr -a gs 'git status'
+abbr -a gl 'git log --oneline --decorate --graph'
+abbr -a gd 'git diff'
+
+# Stage / unstage
+abbr -a ga 'git add'
+abbr -a grs 'git restore --staged'
+
+# Commit
+abbr -a gc 'git commit'
+abbr -a gcm 'git commit -m'
+abbr -a gca 'git commit --amend'
+
+# Branch & switch (modern Git)
+abbr -a gb 'git branch'
+abbr -a gsw 'git switch'
+abbr -a gswc 'git switch -c'
+
+# Sync
+abbr -a gf 'git fetch'
+abbr -a gpl 'git pull'
+abbr -a gps 'git push'
+
+# Repo root (kept because it's genuinely useful)
+abbr -a gr 'cd (git rev-parse --show-toplevel)'
 
 # Competitive programming abbreviations
 # g++-11 compiles pre-compiled stdc++.h the fastest
